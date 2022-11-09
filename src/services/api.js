8fdb2fc096fcec5295698a1f831ca202f46a8836
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-11-09 15:33:47 -0300

    adiciona fuções de fetch

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,26 @@
 export async function getCategories() {
   // Implemente aqui
-  const endPoint = ' https://api.mercadolibre.com/sites/MLB/categories';
+  const endPoint = 'https://api.mercadolibre.com/sites/MLB/categories';
   const response = await fetch(endPoint);
   const data = await response.json();
   return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  if (categoryId) {
+    const endPoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+    const response = await fetch(endPoint);
+    const data = await response.json();
+    return data;
+  }
+
+  if (query) {
+    const endPoint = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+    const response = await fetch(endPoint);
+    const data = await response.json();
+    return data;
+  }
 }
 
 export async function getProductById() {
