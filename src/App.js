dc4b74fa767c56c97c4f7801775a2d479dcefb9a
Iago/ <iago.rodrigues94@hotmail.com>
2022-11-10 15:57:57 -0300

    desenvolvo Componente ProductDetail

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
 import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Switch>
       <Route exact path="/" component={ Home } />
       <Route exact path="/shoppingcart" component={ ShoppingCart } />
+      <Route exact path="/product/:id" component={ ProductDetail } />
     </Switch>
   );
 }
